05a99b25a315b28cd04bcbbb6a6807ec207a5fa9
unknown <gokussjindia@gmail.com>
2024-07-02 23:33:52 +0530

    Getuser list API implementation

--- services/user.service.js
+++ services/user.service.js
@@ -1,6 +1,20 @@
+const { userModel } = require("../models");
+const utils = require("../utils/util");
 class UserService {
   async getUserList(data, next) {
     try {
+      const usersList = await utils.findAllRecords(
+        userModel,
+        { status: "ACTIVE" },
+        { name: 1, email: 1, address: 1 }
+      );
+      return {
+        success: usersList.length,
+        data: usersList,
+        message: usersList.length
+          ? "Userlist fetched success"
+          : "No data found",
+      };
     } catch (error) {
       next(error.message);
     }
