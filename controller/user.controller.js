00c70e8faac26672cb26bcbbe028c658d7cb9cc5
unknown <gokussjindia@gmail.com>
2024-07-03 23:26:39 +0530

    User controller updations

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,17 +4,17 @@ class UserController {
   async getUserList(req, res, next) {
     try {
       const usersList = await userService.getUserList(req, next);
-      if (usersList.length) {
+      if (usersList.data.length) {
         res.status(200).send({
           success: true,
           message: "Users list fetched successfully",
-          data: usersList,
+          data: usersList.data,
         });
       } else {
         res.status(400).send({
           success: false,
           message: "Error while fetching users list",
-          data: usersList,
+          data: usersList.data,
         });
       }
     } catch (error) {
