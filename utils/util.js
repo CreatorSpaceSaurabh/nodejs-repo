6f2462028af34c0c28579b7ddf47be8c97b52308
unknown <gokussjindia@gmail.com>
2024-07-05 23:15:46 +0530

    Generic DB methods added

--- utils/util.js
+++ utils/util.js
@@ -2,6 +2,16 @@ const findAllRecords = async (model, condition, options) => {
   return await model.find(condition, options);
 };
 
+const findOneRecords = async (model, condition, options) => {
+  return await model.findOne(condition, options);
+};
+
+const save = async (model, obj) => {
+  return await model.create(obj);
+};
+
 module.exports = {
   findAllRecords,
+  findOneRecords,
+  save,
 };
